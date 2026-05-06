dd0fe9d1cbd0a522a4ed18731f8e39793bb22010
Bui Tran Tien <buitrantien246@gmail.com>
2026-05-06 18:40:35 +0700

    firt commit

---
+++ models/contact.js
@@ -0,0 +1,19 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Contact extends Model {}
+  Contact.init({
+    name:         { type: DataTypes.STRING, allowNull: false },
+    phone:        { type: DataTypes.STRING, allowNull: false },
+    email:        { type: DataTypes.STRING, allowNull: false },
+    tour_interest: DataTypes.STRING,
+    description:  DataTypes.TEXT,
+  }, {
+    sequelize,
+    modelName: 'Contact',
+    tableName: 'contacts',
+    timestamps: true,
+  });
+  return Contact;
+};
---
+++ models/user.js
@@ -0,0 +1,47 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    class User extends Model {
+        static associate(models) {
+            User.hasOne(models.Profile, { foreignKey: 'userId', as: 'profile' });
+            User.hasMany(models.Order, { foreignKey: 'user_id', as: 'orders' });
+        }
+    }
+    User.init({
+        username: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false,
+            validate: { isEmail: true }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        phone: DataTypes.STRING,
+        role: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: 'user'
+        },
+        avatar: DataTypes.STRING
+    }, {
+        sequelize,
+        modelName: 'User',
+        tableName: 'Users',
+        timestamps: true,
+        defaultScope: {
+            attributes: { exclude: ['password'] }
+        },
+        scopes: {
+            withPassword: { attributes: {} }
+        }
+    });
+    return User;
+};
